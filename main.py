2aff7cb6543fd43ff85b452b431de54eb8fd738a
TheDocisHere <61443870+Th3DocisH3r3@users.noreply.github.com>
2025-01-10 01:05:26 -0600

    Update main.py

--- main.py
+++ main.py
@@ -9,23 +9,18 @@ import yt_dlp
 import asyncio
 
 dirPath = os.path.realpath(__file__).split("main.py")[0]
-
-print(dirPath)
-
 ytdlp_format_options = {
    'format': 'bestaudio',
    'noplaylist': True,
    'outtmpl': f"{dirPath}Downloaded Music\\%(title)s - %(id)s.%(ext)s"
 }
-
 Fffmpeg_options = {
    'before_options': '-reconnect 1 -reconnected-streamed 1 -reconnect_delay_max 5',
    'options': '-vn'
 }
 
 ytdlp = yt_dlp.YoutubeDL(ytdlp_format_options)
-
-spotifyPlaylistID = "3H3Dfcb6BtIFP0imQzi8fY"
+spotifyPlaylistID = "23QwK2nByY7jlcTV24G1aE"
 
 #Reads previously downloaded songs
 downloaded_songs = {}
@@ -33,10 +28,11 @@ for file in os.listdir(f"{dirPath}Downloaded Music"):
    downloaded_songs[file.split("- ")[-1][:-4]] = {"file":f"{dirPath}\\Downloaded Music\\{file}","title":file.split(" - " + file.split(" - ")[-1])[0]}
 
 print(f"Found {len(downloaded_songs)} downloaded songs")
-#Opens file in same folder named secret.txt to read bot secret
+#Grab bot secret
 with open(dirPath + "botsecret.txt", "r") as botFile:
    botSecret = botFile.readline() # Put Bot Secret on first line
    botFile.close()
+#Grab spotify client id and secret
 with open(dirPath + "spotifysecret.txt", "r") as spotifyFile:
    spotifyID = spotifyFile.readline()[:-1] # Put Spotify Client Secret on first line
    spotifySecret = spotifyFile.readline() # Put Spotify Client ID on second line
@@ -54,9 +50,7 @@ for song in sp.playlist(spotifyPlaylistID,"tracks")["tracks"]["items"]:
    artists = artists.removesuffix(",")
    video = youtube_search.YoutubeSearch(f"{song["track"]["name"]} - {artists}",max_results=1).to_dict()[0]
    videoID = video["id"]
-   if videoID in downloaded_songs.keys():
-      pass
-   else:
+   if not videoID in downloaded_songs.keys():
       info = ytdlp.extract_info(f"https://www.youtube.com/watch?v={videoID}",download=True)
       filename = ytdlp.prepare_filename(info)
       downloaded_songs[videoID] = {"file":filename,"title":info["title"]}
@@ -85,18 +79,15 @@ async def start():
    global currentSong
    voice = discord.utils.get(bot.voice_clients, guild=bot.get_guild(discordServerID))
    VoiceChannel = bot.get_channel(discordVoiceChannelID)
-   print(voice)
-   print(VoiceChannel)
    #Join voice channel
    if not voice or not voice.is_connected():
       voice = await VoiceChannel.connect()
-   print(voice)
    currentSong += 1
    if currentSong >= len(songQueue):
       currentSong = 0
       random.shuffle(songQueue)
    source = discord.FFmpegOpusAudio(songQueue[currentSong]["file"],executable="C:/Program Files/ffmpeg-master-latest-win64-gpl/bin/ffmpeg.exe")
    voice.play(source,after=lambda e: asyncio.run_coroutine_threadsafe(start(), bot.loop))
-   await bot.change_presence(activity=discord.CustomActivity(f"Playing {song["title"]}"))
+   await bot.change_presence(activity=discord.CustomActivity(f"Playing {songQueue[currentSong]["title"]}"))
 
 bot.run(botSecret)
